a38720ea46b1c819896c098801840abbe671766a
julianlarra <larragueta.julian@gmail.com>
2022-04-14 23:37:23 -0300

    tp

---
+++ UNIDAD2/FICHA05/02.py
@@ -0,0 +1,10 @@
+import random
+cartas_numero =  3, 11,
+cartas_palo = "corazones", "pircas", "trebol", "rombo",
+ass = 1
+carta_1 = random.choice(cartas_numero), random.choice(cartas_palo)
+
+print(carta_1)
+if carta_1[0] == 11 :
+   carta_1 = ass , carta_1[1]
+print(carta_1)
